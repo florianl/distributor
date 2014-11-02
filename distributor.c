e2412efc86e725d102729e64a3d697e56a111c12
Lehner Florian <dev@der-flo.net>
2014-11-02 19:57:17 +0100

    start threading
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- distributor.c
+++ distributor.c
@@ -31,17 +31,48 @@
 #include <string.h>
 
 #define     NUM_WORKER      sysconf(_SC_NPROCESSORS_ONLN)
+#define     PIPE_BUF        2048
 
 struct kit {
     pthread_t       tID;
     int             wID;
+    int             fd[2];
 };
 
 static void *processing(void *arg){
 
     struct kit      *tool = arg;
+    char            buf[PIPE_BUF];
 
     fprintf(stdout, "%d %d\n", (int) tool->tID, tool->wID);
+
+    close(tool->fd[1]);     /*  Close the writing end of the pipe   */
+
+    while(read(tool->fd[0], &buf, PIPE_BUF) > 0){
+        fprintf(stdout, "[%d] %s\n", tool->wID, buf);
+    }
+    close(tool->fd[0]);
+
+    return NULL;
+}
+
+static void *distribute(void *arg){
+
+    struct kit      *tool = arg;
+    int             i = 0;
+
+    for(i=0; i<NUM_WORKER; i++){
+        close(tool[i].fd[0]);   /*  Close the reading end of the pipe   */
+    }
+
+    while(1){
+        write(tool[i%NUM_WORKER].fd[1], (void *) ((48+(i%NUM_WORKER))%58), sizeof(char));
+        i++;
+    }
+
+    for(i=0; i<NUM_WORKER; i++){
+        close(tool[i].fd[1]);
+    }
 
     return NULL;
 }
@@ -51,16 +82,49 @@ int main(int argc, char *argv[]){
     int             i = 0;
     int             status = 0;
     struct kit      *tool;
+    pthread_t       distributor;
 
     tool = calloc(NUM_WORKER, sizeof(struct kit));
     if(tool == NULL){
         return EXIT_FAILURE;
     }
 
+    fprintf(stdout, "%ld worker will start\n", NUM_WORKER);
+
     for(i=0; i<NUM_WORKER; i++){
         tool[i].wID = i;
+        status = pipe(tool[i].fd);
+        if(status){
+            fprintf(stderr, "%s\n", strerror(errno));
+            return EXIT_FAILURE;
+        }
+
 
         status = pthread_create(&tool[i].tID, NULL, &processing, &tool[i]);
+
+        if(status){
+            fprintf(stderr, "%s\n", strerror(status));
+            return EXIT_FAILURE;
+        }
+    }
+
+    status = pthread_create(&distributor, NULL, &distribute, &tool);
+
+    if(status){
+        fprintf(stderr, "%s\n", strerror(status));
+        return EXIT_FAILURE;
+    }
+
+    status = pthread_join(distributor, NULL);
+
+    if(status){
+        fprintf(stderr, "%s\n", strerror(status));
+        return EXIT_FAILURE;
+    }
+
+    for(i=0; i<NUM_WORKER; i++){
+
+        status = pthread_join(tool[i].tID, NULL);
 
         if(status){
             fprintf(stderr, "%s\n", strerror(status));
